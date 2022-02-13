72b1568f512892c78d6fb98ee9254eae28e01ec3
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2022-02-13 19:33:00 +0200

    Added functionality with more conditional rendering to show/hide punchline with a toggle button

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 export default function Joke(props) {
   const {num, setup, punchline} = props;
-  
   const ids = [num + 1, num + 1, (num + 1) * Math.random()];
+  const [isShown, setIsShown] = React.useState(false);
+
+  function toggle() {
+    setIsShown(prevBool => !prevBool);
+  }
 
   return (
     <ul key={ids[0]} className="joke">
       {setup && <li key={ids[1]} className="setup">{setup}</li>}
-      <li key={ids[2]} className="punchline">{punchline}</li>
+      {isShown && <li key={ids[2]} className="punchline">{punchline}</li>}
+      <button onClick={toggle}>Show Punchline</button>
     </ul>
   );
 };
