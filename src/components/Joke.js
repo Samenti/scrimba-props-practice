6f8967759f08bd6052146131ffef93d35e2a730e
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2022-02-13 19:19:38 +0200

    Conditional rendering as a better way to deal with jokes without setup line

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -4,16 +4,10 @@ export default function Joke(props) {
   const {num, setup, punchline} = props;
   
   const ids = [num + 1, num + 1, (num + 1) * Math.random()];
-  let setupLine;
-  if (setup == null) {
-    setupLine = <></>;
-  } else {
-    setupLine = <li key={ids[1]} className="setup">{setup}</li>;
-  }
 
   return (
     <ul key={ids[0]} className="joke">
-      {setupLine}
+      {setup && <li key={ids[1]} className="setup">{setup}</li>}
       <li key={ids[2]} className="punchline">{punchline}</li>
     </ul>
   );
